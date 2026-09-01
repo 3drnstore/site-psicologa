a4add2d2b8430ca6ffd5d2a3371d857bbb734b29
3drnstore <thiagodrn2@gmail.com>
2026-09-01 17:54:37 -0300

    Route /admin/setup to initial admin setup screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AdminSetup from './AdminSetup'
 import './styles.css'
 import './app-extra.css'
 import './v2.css'
 
+const Root = window.location.pathname === '/admin/setup' ? AdminSetup : App
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
