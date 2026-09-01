0611bc2392224afc83ebf862e2d464ae509b5bce
3drnstore <thiagodrn2@gmail.com>
2026-09-01 19:48:26 -0300

    Enable professional calendar enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from './App'
 import AdminSetup from './AdminSetup'
 import PasswordRecovery from './PasswordRecovery'
 import { installPasswordEnhancer } from './password-enhancer'
+import { installAdminCalendarEnhancer } from './admin-calendar-enhancer'
 import './styles.css'
 import './app-extra.css'
 import './v2.css'
+import './admin-calendar.css'
 
 const path = window.location.pathname
 
@@ -20,3 +22,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><RoutedApp /></React.StrictMode>,
 )
 installPasswordEnhancer()
+installAdminCalendarEnhancer()
